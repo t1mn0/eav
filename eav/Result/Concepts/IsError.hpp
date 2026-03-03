b9b5a3eb376e82b5d57e6cfda55e48b2d440921d
t1mn0 <timofeyalbecov@gmail.com>
2026-03-03 13:53:30 +0300

    [IsError concept]: mitigation requirements for Err type

--- eav/Result/Concepts/IsError.hpp
+++ eav/Result/Concepts/IsError.hpp
@@ -13,7 +13,6 @@ concept IsError =
     (std::move_constructible<E> &&
      std::destructible<E> &&
      !std::is_void_v<E> &&
-     !std::is_array_v<E> &&
-     !std::is_reference_v<E>);
+     !std::is_array_v<E>);
 
 }  // namespace eav::concepts
